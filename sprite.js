IceCreamYou/Canvas-Sprite-Animations

commit 24c84488ad625a5435d3b8b956c576cc6588e380

Added support for using Images and Canvases as the Sprite source image (in addition to a string image file path); Added pre-rendering for flipped sequences

sprite.js
@@ -21,7 +21,11 @@
  *   Creates a new SpriteMap instance.
  *
  * @param {String} src
- *   The file path of the base image.
+ *   The image to draw, in the form of one of the following:
+ *
+ *   - The file path of the base image
+ *   - An Image
+ *   - A Canvas
  * @param {Object} animations
  *   A map (Object) where the keys are the names of animation sequences and the
  *   values are maps (Objects) specifying the starting and ending frames of the
@@ -56,10 +60,11 @@
  */
 function SpriteMap(src, animations, options) {
   this.sprite = new Sprite(src, options);
+  this.baseImage = this.sprite.image;
+  this.cachedImages = {'00': this.baseImage};
   this.sprite.spriteMap = this;
   this.maps = {};
-  var name;
-  for (name in animations) {
+  for (var name in animations) {
     if (animations.hasOwnProperty(name)) {
       this.set(name, animations[name]);
     }
@@ -92,8 +97,10 @@ SpriteMap.prototype = {
    *   row in the Sprite until reaching the last frame in the loop. More
    *   details on how this work are documented in the {@link Sprite}
    *   constructor.
-   * @param {Boolean[]} [options.flipped]
-   *   Indicates whether the Sprite should be flipped when drawn.
+   * @param {Object} [options.flipped={horizontal: false, vertical: false}]
+   *   An object with "horizontal" and "vertical" properties (both Booleans)
+   *   indicating whether the Sprite should be drawn flipped along the
+   *   horizontal or vertical axes.
    * @param {Boolean} [options.flipped.horizontal=false]
    * @param {Boolean} [options.flipped.vertical=false]
    */
@@ -114,8 +121,14 @@ SpriteMap.prototype = {
         endRow:   typeof options.endRow   !== 'undefined' ? options.endRow   : this.sprite.rows-1,
         endCol:   typeof options.endCol   !== 'undefined' ? options.endCol   : this.sprite.cols-1,
         squeeze:  typeof options.squeeze  !== 'undefined' ? options.squeeze  : false,
-        flipped:  typeof options.flipped  !== 'undefined' ? options.flipped  : [false, false]
+        flipped:  typeof options.flipped  !== 'undefined' ? options.flipped  : {horizontal: false, vertical: false}
     };
+    var f = this.maps[name].flipped,
+        key = (f.horizontal ? '1' : '0') + (f.vertical ? '1' : '0');
+    if (typeof this.cachedImages[key] === 'undefined') {
+      this.cachedImages[key] = this.sprite._prerender(this.baseImage, f);
+    }
+    this.maps[name].image = this.cachedImages[key];
     return this;
   },
   /**
@@ -150,6 +163,9 @@ SpriteMap.prototype = {
     this.activeLoop = name;
     var m = this.maps[name];
     this.sprite.setLoop(m.startRow, m.startCol, m.endRow, m.endCol, m.squeeze, m.flipped);
+    if (m.image) {
+      this.sprite.image = m.image;
+    }
     return this;
   },
   /**
@@ -267,7 +283,11 @@ this.SpriteMap = SpriteMap;
  *   Creates a new Sprite instance.
  *
  * @param {String} src
- *   The file path of the base image.
+ *   The image to draw, in the form of one of the following:
+ *
+ *   - The file path of the base image
+ *   - An Image
+ *   - A Canvas
  * @param {Object} [options]
  *   An object whose properties affect how the sprite is animated. Each of the
  *   properties will be attached to the Sprite object directly, along with
@@ -350,19 +370,50 @@ this.SpriteMap = SpriteMap;
  *   The Sprite that was loaded. 
  */
 function Sprite(src, options) {
-  this.sourceFile = src;
-  var cachedImage = Sprite.getImageFromCache(src);
-  if (cachedImage) {
-    this._init(cachedImage, options);
+  // String image file path
+  if (typeof src == 'string') {
+    this.sourceFile = src;
+    var cachedImage = Sprite.getImageFromCache(src);
+    if (cachedImage) { // cached
+      this._init(cachedImage, options);
+    }
+    else { // not cached
+      var image = new Image(), t = this;
+      image.onload = function() {
+        t._init(this, options);
+      };
+      image._src = src;
+      image.src = src;
+      Sprite.saveImageToCache(src, image);
+    }
   }
-  else {
-    var image = new Image(), t = this;
-    image.onload = function() {
-      t._init(this, options);
-    };
-    image._src = src;
-    image.src = src;
-    Sprite.saveImageToCache(src, image);
+  // Image
+  else if (src instanceof HTMLImageElement || src instanceof Image) {
+    if (!src.src) {
+      return;
+    }
+    this.sourceFile = src._src || src.src;
+    if (src.complete || (src.width && src.height)) { // loaded
+      Sprite.saveImageToCache(this.sourceFile, src);
+      this._init(src, options);
+    }
+    else { // not loaded
+      if (src._src) { // We've already tried to draw this one
+        return; // The onload callback will initialize the sprite when it runs
+      }
+      var o = src.onload, t = this;
+      src.onload = function() {
+        if (typeof o == 'function') { // don't overwrite any existing handler
+          o();
+        }
+        Sprite.saveImageToCache(t.sourceFile, src);
+        t._init(this, options);
+      };
+    }
+  }
+  // Canvas
+  else if (src instanceof HTMLCanvasElement) {
+    this._init(src, options);
   }
 }
 Sprite.prototype = {
@@ -402,6 +453,28 @@ Sprite.prototype = {
     }
   },
   /**
+   * Pre-render the image onto a canvas.
+   *
+   * Canvases typically draw faster than images, especially when flipped.
+   *
+   * @return {HTMLCanvasElement} The prerendered flipped image.
+   * @ignore
+   */
+  _prerender: function(image, flipped) {
+    var tempCanvas = document.createElement('canvas');
+    tempCanvas.width = this.width;
+    tempCanvas.height = this.height;
+    var tempContext = tempCanvas.getContext('2d');
+    if (flipped.horizontal || flipped.vertical) {
+      tempContext.translate(flipped.horizontal ? tempCanvas.width : 0,
+          flipped.vertical ? tempCanvas.height : 0);
+      tempContext.scale(flipped.horizontal ? -1 : 1,
+          flipped.vertical ? -1 : 1);
+    }
+    tempContext.drawImage(image, 0, 0);
+    return tempCanvas;
+  },
+  /**
    * Draws the sprite.
    *
    * @param {CanvasRenderingContext2D} ctx
@@ -426,21 +499,24 @@ Sprite.prototype = {
       ctx.save();
       w = w || this.projectedW;
       h = h || this.projectedH;
-      if (this.flipped.horizontal || this.flipped.vertical) {
-        ctx.scale(this.flipped.horizontal ? -1 : 1, this.flipped.vertical ? -1 : 1);
-        if (this.flipped.horizontal) x = -x - w;
-        if (this.flipped.vertical) y = -y - h;
+      var xOffset = this.col * this.frameW,
+          yOffset = this.row * this.frameH;
+      if (this.flipped.horizontal) {
+        xOffset = this.width - xOffset - this.frameW;
+      }
+      if (this.flipped.vertical) {
+        yOffset = this.height - yOffset - this.frameH;
       }
       ctx.drawImage(
-          this.image,             // image
-          this.col * this.frameW, // image x-offset
-          this.row * this.frameH, // image y-offset
-          this.frameW,            // image slice width
-          this.frameH,            // image slice height
-          x,                      // canvas x-position
-          y,                      // canvas y-position
-          w,                      // slice width on canvas
-          h                       // slice height on canvas
+          this.image,  // image
+          xOffset,     // image x-offset
+          yOffset,     // image y-offset
+          this.frameW, // image slice width
+          this.frameH, // image slice height
+          x,           // canvas x-position
+          y,           // canvas y-position
+          w,           // slice width on canvas
+          h            // slice height on canvas
       );
       ctx.restore();
     } catch(e) {

docs/index.html
@@ -44,7 +44,7 @@
         </div>
 
   
-  <div id='footer-content' style='display: none'>By <a href=http://www.isaacsukin.com/>Isaac Sukin</a> (<a href=https://github.com/IceCreamYou>IceCreamYou</a>)</div>
+  <div id='footer-content' style='display: none'>By <a href="http://www.isaacsukin.com/">Isaac Sukin</a> (<a href="https://github.com/IceCreamYou">IceCreamYou</a>)</div>
 
   
 

docs/source/data-69f06f57ecc11dac8a525534e23a2c6b.html
@@ -1,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>The source code</title>
+  <link href="../resources/prettify/prettify.css" type="text/css" rel="stylesheet" />
+  <script type="text/javascript" src="../resources/prettify/prettify.js"></script>
+  <style type="text/css">
+    .highlight { display: block; background-color: #ddd; }
+  </style>
+  <script type="text/javascript">
+    function highlight() {
+      document.getElementById(location.hash.replace(/#/, "")).className = "highlight";
+    }
+  </script>
+</head>
+<body onload="prettyPrint(); highlight();">
+  <pre class="prettyprint lang-js">Docs = {&quot;data&quot;:{&quot;classes&quot;:[{&quot;name&quot;:&quot;SpriteMap&quot;,&quot;extends&quot;:null,&quot;private&quot;:null,&quot;icon&quot;:&quot;icon-class&quot;},{&quot;name&quot;:&quot;Sprite&quot;,&quot;extends&quot;:null,&quot;private&quot;:null,&quot;icon&quot;:&quot;icon-class&quot;}],&quot;guides&quot;:[],&quot;videos&quot;:[],&quot;examples&quot;:[],&quot;search&quot;:[{&quot;name&quot;:&quot;SpriteMap&quot;,&quot;fullName&quot;:&quot;SpriteMap&quot;,&quot;icon&quot;:&quot;icon-class&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap&quot;,&quot;meta&quot;:{},&quot;sort&quot;:1},{&quot;name&quot;:&quot;constructor&quot;,&quot;fullName&quot;:&quot;SpriteMap.constructor&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-constructor&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;set&quot;,&quot;fullName&quot;:&quot;SpriteMap.set&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-set&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;unset&quot;,&quot;fullName&quot;:&quot;SpriteMap.unset&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-unset&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;use&quot;,&quot;fullName&quot;:&quot;SpriteMap.use&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-use&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;activeLoop&quot;,&quot;fullName&quot;:&quot;SpriteMap.activeLoop&quot;,&quot;icon&quot;:&quot;icon-property&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-property-activeLoop&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;start&quot;,&quot;fullName&quot;:&quot;SpriteMap.start&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-start&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;stop&quot;,&quot;fullName&quot;:&quot;SpriteMap.stop&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-stop&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;reset&quot;,&quot;fullName&quot;:&quot;SpriteMap.reset&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-reset&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;runOnce&quot;,&quot;fullName&quot;:&quot;SpriteMap.runOnce&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-runOnce&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;draw&quot;,&quot;fullName&quot;:&quot;SpriteMap.draw&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-draw&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;clone&quot;,&quot;fullName&quot;:&quot;SpriteMap.clone&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/SpriteMap-method-clone&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;Sprite&quot;,&quot;fullName&quot;:&quot;Sprite&quot;,&quot;icon&quot;:&quot;icon-class&quot;,&quot;url&quot;:&quot;#!/api/Sprite&quot;,&quot;meta&quot;:{},&quot;sort&quot;:1},{&quot;name&quot;:&quot;constructor&quot;,&quot;fullName&quot;:&quot;Sprite.constructor&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-constructor&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;draw&quot;,&quot;fullName&quot;:&quot;Sprite.draw&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-draw&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;reset&quot;,&quot;fullName&quot;:&quot;Sprite.reset&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-reset&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;changeFrame&quot;,&quot;fullName&quot;:&quot;Sprite.changeFrame&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-changeFrame&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;setFrame&quot;,&quot;fullName&quot;:&quot;Sprite.setFrame&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-setFrame&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;setLoop&quot;,&quot;fullName&quot;:&quot;Sprite.setLoop&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-setLoop&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;startLoop&quot;,&quot;fullName&quot;:&quot;Sprite.startLoop&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-startLoop&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;stopLoop&quot;,&quot;fullName&quot;:&quot;Sprite.stopLoop&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-stopLoop&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;runLoop&quot;,&quot;fullName&quot;:&quot;Sprite.runLoop&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-runLoop&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;prevFrame&quot;,&quot;fullName&quot;:&quot;Sprite.prevFrame&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-prevFrame&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;nextFrame&quot;,&quot;fullName&quot;:&quot;Sprite.nextFrame&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-nextFrame&quot;,&quot;meta&quot;:{&quot;chainable&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;getFrame&quot;,&quot;fullName&quot;:&quot;Sprite.getFrame&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-getFrame&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;getNumFrames&quot;,&quot;fullName&quot;:&quot;Sprite.getNumFrames&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-getNumFrames&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;frameNumberToRowCol&quot;,&quot;fullName&quot;:&quot;Sprite.frameNumberToRowCol&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-frameNumberToRowCol&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;clone&quot;,&quot;fullName&quot;:&quot;Sprite.clone&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-method-clone&quot;,&quot;meta&quot;:{},&quot;sort&quot;:3},{&quot;name&quot;:&quot;getImageFromCache&quot;,&quot;fullName&quot;:&quot;Sprite.getImageFromCache&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-static-method-getImageFromCache&quot;,&quot;meta&quot;:{&quot;static&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;saveImageToCache&quot;,&quot;fullName&quot;:&quot;Sprite.saveImageToCache&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-static-method-saveImageToCache&quot;,&quot;meta&quot;:{&quot;static&quot;:true},&quot;sort&quot;:3},{&quot;name&quot;:&quot;preloadImages&quot;,&quot;fullName&quot;:&quot;Sprite.preloadImages&quot;,&quot;icon&quot;:&quot;icon-method&quot;,&quot;url&quot;:&quot;#!/api/Sprite-static-method-preloadImages&quot;,&quot;meta&quot;:{&quot;static&quot;:true},&quot;sort&quot;:3}],&quot;tests&quot;:false,&quot;signatures&quot;:[{&quot;long&quot;:&quot;abstract&quot;,&quot;short&quot;:&quot;ABS&quot;,&quot;key&quot;:&quot;abstract&quot;},{&quot;long&quot;:&quot;chainable&quot;,&quot;short&quot;:&quot;&amp;gt;&quot;,&quot;key&quot;:&quot;chainable&quot;},{&quot;long&quot;:&quot;deprecated&quot;,&quot;short&quot;:&quot;DEP&quot;,&quot;key&quot;:&quot;deprecated&quot;},{&quot;long&quot;:&quot;&amp;#9733;&quot;,&quot;short&quot;:&quot;&amp;#9733;&quot;,&quot;key&quot;:&quot;new&quot;},{&quot;long&quot;:&quot;preventable&quot;,&quot;short&quot;:&quot;PREV&quot;,&quot;key&quot;:&quot;preventable&quot;},{&quot;long&quot;:&quot;private&quot;,&quot;short&quot;:&quot;PRI&quot;,&quot;key&quot;:&quot;private&quot;},{&quot;long&quot;:&quot;protected&quot;,&quot;short&quot;:&quot;PRO&quot;,&quot;key&quot;:&quot;protected&quot;},{&quot;long&quot;:&quot;readonly&quot;,&quot;short&quot;:&quot;R O&quot;,&quot;key&quot;:&quot;readonly&quot;},{&quot;long&quot;:&quot;removed&quot;,&quot;short&quot;:&quot;REM&quot;,&quot;key&quot;:&quot;removed&quot;},{&quot;long&quot;:&quot;required&quot;,&quot;short&quot;:&quot;REQ&quot;,&quot;key&quot;:&quot;required&quot;},{&quot;long&quot;:&quot;static&quot;,&quot;short&quot;:&quot;STA&quot;,&quot;key&quot;:&quot;static&quot;},{&quot;long&quot;:&quot;template&quot;,&quot;short&quot;:&quot;TMP&quot;,&quot;key&quot;:&quot;template&quot;}],&quot;localStorageDb&quot;:&quot;jsduckcsa&quot;,&quot;showPrintButton&quot;:false,&quot;touchExamplesUi&quot;:false,&quot;source&quot;:true,&quot;commentsUrl&quot;:null,&quot;commentsDomain&quot;:null}};
+</pre>
+</body>
+</html>

commit 610ca7a5348f836e01b4ac2b13caf52786924528

Fixed SpriteMap fails if image is not pre-loaded (bug introduced in 24c8448)

sprite.js
@@ -59,16 +59,23 @@
  *   class.
  */
 function SpriteMap(src, animations, options) {
-  this.sprite = new Sprite(src, options);
-  this.baseImage = this.sprite.image;
-  this.cachedImages = {'00': this.baseImage};
-  this.sprite.spriteMap = this;
-  this.maps = {};
-  for (var name in animations) {
-    if (animations.hasOwnProperty(name)) {
-      this.set(name, animations[name]);
+  var origPIC = typeof options.postInitCallback == 'function' ? options.postInitCallback : null;
+  var t = this;
+  options.postInitCallback = function() {
+    if (origPIC) {
+      t.baseImage = t.sprite.image;
+      t.cachedImages = {'00': t.baseImage};
+      t.maps = {};
+      for (var name in animations) {
+        if (animations.hasOwnProperty(name)) {
+          t.set(name, animations[name]);
+        }
+      }
+      origPIC.apply(this, arguments);
     }
-  }
+  };
+  this.sprite = new Sprite(src, options);
+  this.sprite.spriteMap = this;
 }
 SpriteMap.prototype = {
   /**